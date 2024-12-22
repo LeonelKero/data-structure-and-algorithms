4dd5fc9be6e78629a107a6ff19f82d7e7c87c64f
leonel ka <waboleonel@gmail.com>
2024-12-22 20:54:24 +0100

    code refactoring doc

--- src/datastructures/linkedlist/Node.java
+++ src/datastructures/linkedlist/Node.java
@@ -1,7 +1,8 @@
 package datastructures.linkedlist;
 
 /**
- * Represents a node of a simple linked list. It holds a value of type Integer and point to the next node.
+ * Represents a node of a simple linked list.
+ * It holds a value of type Integer and point to the next node.
  * The next node can be null.
  */
 class Node {
